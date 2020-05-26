338fc2500970a0d9c49713c3abecf6cfc5478b3b
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-05-26 06:40:19 +0100

    CORS

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/saiprasadkrishnamurthy/reference-data-api/config"
 	"github.com/saiprasadkrishnamurthy/reference-data-api/controllers"
@@ -10,6 +12,18 @@ const apiBase = "/api"
 
 // InitialiseAllRoutes initialises all routes in the API.
 func InitialiseAllRoutes(r *httprouter.Router) {
+	// CORS.
+	r.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Access-Control-Request-Method") != "" {
+			// Set CORS headers
+			header := w.Header()
+			header.Set("Access-Control-Allow-Methods", r.Header.Get("Allow"))
+			header.Set("Access-Control-Allow-Origin", "*")
+		}
+
+		// Adjust status code to 204
+		w.WriteHeader(http.StatusNoContent)
+	})
 	apiContext := apiBase + "/" + config.APIVersion()
 	appController := controllers.BaseController{}
 
